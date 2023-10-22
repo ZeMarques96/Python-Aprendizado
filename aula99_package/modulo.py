99508ad96f24fa7dcd0e7671d2f21c9fb89fac32
José Francisco Marques Neto <netomarques96@gmail.com>
2023-10-22 03:53:07 -0300

    movendo a pasta package

---
+++ aula99_package/modulo.py
@@ -0,0 +1,14 @@
+# __all__ = [
+#     'variavel',
+#     'soma_do_produto',
+# ]
+
+from aula99_package.modulo_b import fala_oi
+
+
+variavel = 'alguma coisa'
+
+def soma_do_modulo(x, y):
+    return x +y
+
+fala_oi()
