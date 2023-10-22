422ffaa3e1fdfa7cadf7b9831381160f511a0b07
José Francisco Marques Neto <netomarques96@gmail.com>
2023-10-22 14:53:43 -0300

    Exercícios 89 finalizado

--- Professor Guanabara/ex89.py
+++ Professor Guanabara/ex89.py
@@ -1,20 +1,55 @@
 alunos = [
-        ['Aluno 1', ['nota 1', 'nota 2'], ['Média']], 
-        ['Aluno 2', ['nota 1', 'nota 2'], ['Média']],
-        ['Aluno 3', ['nota 1', 'nota 2'], ['Média']],
+        # ['Aluno 1', [6.7, 4.5], 5.8], 
+        # ['Aluno 2', [7.3,  8.2], 6.2], 
+        # ['Aluno 3', [ 6.1, 4.5], 3.8], 
         ]
-# print(alunos[0][0])
-# print(alunos[0][1])
-# print(alunos[0][2])
-# print(alunos[0][1][1])
-for aluno in alunos:
-    for chave in range(len(aluno)):
-        if chave == 0:
-            aluno[chave] = str(input('Digite o seu nome: '))
-            print(aluno[chave])
-        if chave == 1:
-            for nota in range(len(aluno[chave])):
-                aluno[chave][nota] = float(input(f'Nota {nota + 1}:'))
-                print(nota)
+while True:
+    aluno = []
+    notas = []
+    nome = str(input('Digite o seu nome: '))
+    aluno.append(nome)
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    notas.append(nota1)
+    notas.append(nota2)
+    media = (nota1 + nota2) / 2
+    aluno.append(notas)
+    aluno.append(media)
+    alunos.append(aluno)
+    while True:
+        resposta = str(input('Deseja continuar? [S/N]'))
+        if resposta in 'Nn':
+            break
+        elif resposta in 'Ss':
+            break
+        else:
+            print('Resposta inválida!!')
+    if resposta in 'Nn':
+        break
 
-print(alunos)
+print(f"No.    Nome     {'MÉDIA':>15}")
+print('-'*30)
+for i in range(len(alunos)):
+    print( f'{i:<5}' , end=' ')
+    for info in range(len(alunos[i])):
+        if info == 0:
+            print(f"{alunos[i][info]:>5}", end='  ')
+        if info == 2:
+            print( f"{alunos[i][info]:>15}" , end= '')
+    print('\n')
+while True:
+    resposta = 0
+    resposta = int(input('Deseja ver as notas de qual aluno? '))
+    if resposta >= len(alunos) and resposta != 999:
+        print('Resposta inválida, aluno não existente! ')
+        continue
+    if resposta == 999:
+        print('Programa encerrado!')
+        break
+    print(alunos[resposta][0], alunos[resposta][1])
+
+
+
+
+
+# print(alunos)
