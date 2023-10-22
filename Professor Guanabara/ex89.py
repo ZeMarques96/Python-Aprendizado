6834423411608bbac7d09d0dd54b1a2cb7703351
José Francisco Marques Neto <netomarques96@gmail.com>
2023-10-22 14:13:18 -0300

    resolução parcial

--- Professor Guanabara/ex89.py
+++ Professor Guanabara/ex89.py
@@ -1,5 +1,20 @@
-alunos = [['Aluno', ['nota 1', 'nota 2'], ['Média']]]
-print(alunos[0][0])
-print(alunos[0][1])
-print(alunos[0][2])
-print(alunos[0][1][1])
+alunos = [
+        ['Aluno 1', ['nota 1', 'nota 2'], ['Média']], 
+        ['Aluno 2', ['nota 1', 'nota 2'], ['Média']],
+        ['Aluno 3', ['nota 1', 'nota 2'], ['Média']],
+        ]
+# print(alunos[0][0])
+# print(alunos[0][1])
+# print(alunos[0][2])
+# print(alunos[0][1][1])
+for aluno in alunos:
+    for chave in range(len(aluno)):
+        if chave == 0:
+            aluno[chave] = str(input('Digite o seu nome: '))
+            print(aluno[chave])
+        if chave == 1:
+            for nota in range(len(aluno[chave])):
+                aluno[chave][nota] = float(input(f'Nota {nota + 1}:'))
+                print(nota)
+
+print(alunos)
