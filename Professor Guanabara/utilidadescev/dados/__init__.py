170e75506d2de46d932d70ac2b40a619ac8d74da
José Francisco Marques Neto <netomarques96@gmail.com>
2023-11-01 02:27:51 -0300

    Correção finalizada

--- Professor Guanabara/utilidadescev/dados/__init__.py
+++ Professor Guanabara/utilidadescev/dados/__init__.py
@@ -1,6 +1,6 @@
 def validador(string):
     while True:
-        preco = str(input(string))
+        preco = str(input(string)).strip()
         validar = preco.replace(',', '').replace('.', '')
         if validar.isalpha() or validar == '':
             print(f'ERRO: "{preco}" é um preço inválido!')
