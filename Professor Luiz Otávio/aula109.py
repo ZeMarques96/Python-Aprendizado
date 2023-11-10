45b5338b77b7f6d0c92c5a8899eca210be601c92
José Francisco Marques Neto <netomarques96@gmail.com>
2023-11-10 15:26:50 -0300

    Itertools, count

--- Professor Luiz Otávio/aula109.py
+++ Professor Luiz Otávio/aula109.py
@@ -1,3 +1,17 @@
 # count é um iterador sem fim
 
 from itertools import count
+
+
+c1 = count()
+
+
+print(hasattr(c1, '__iter__'))
+# for i in range(100):
+#     print(next(c1))
+
+for i in c1:
+    print(i)
+    if i == 15:
+        break
+
---
+++ Professor Luiz Otávio/aula110.py
@@ -0,0 +1,13 @@
+# Combinations, Permutations e Product - Itertools
+# Combinação - Ordem não importa - iterável + tamanho do grupo
+# Permutação - Ordem importa
+# Produto - Ordem importa e repete valores únicos
+
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Letícia'
+]
+
+camisetas = [
+    ['preta', 'branca']
+]
